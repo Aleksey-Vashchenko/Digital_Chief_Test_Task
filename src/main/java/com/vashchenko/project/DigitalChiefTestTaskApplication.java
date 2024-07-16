b917c399a23a24b4d32a7e9a683375fbeac36d6c
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-07-16 15:43:49 +0300

    init: Init commit

---
+++ src/main/java/com/vashchenko/project/DigitalChiefTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.vashchenko.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalChiefTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalChiefTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vashchenko/project/DigitalChiefTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vashchenko.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalChiefTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
